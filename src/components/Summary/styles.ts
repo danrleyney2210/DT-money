c7ef8152dc443051ab9637e35b41ddcf6bf1d8f6
Danrley Santos <danrleypow@gmail.com>
2021-05-22 12:46:57 -0300

    Uploalding all archive

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -8rem;
+  font-family: 'Poppins', sans-serif;
+
+
+  div {
+    background: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--text-title);
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    strong {
+      margin-top: 1rem;
+      font-size: 2rem;
+      font-weight: 500;
+      line-height: 3rem;
+    }
+
+    &.hightlight-background {
+      background: var(--green);
+      color: #fff;
+    }
+  }
+`;
